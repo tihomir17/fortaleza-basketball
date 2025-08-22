cdfc0ea45b02b31ddaa3cff1c9db51aac7d37887
Tihomir Losic <Tihomir.Losic@endava.com>
2025-08-22 11:27:47 +0200

    Fix calendar view, add tests

--- backend/apps/events/views.py
+++ backend/apps/events/views.py
@@ -1,13 +1,15 @@
-# apps/events/views.py
+# backend/apps/events/views.py
+
 from rest_framework import viewsets, permissions
 from .models import CalendarEvent
 from .serializers import CalendarEventSerializer
 from django.db.models import Q
-
+from apps.teams.models import Team # Import the Team model
 
 class CalendarEventViewSet(viewsets.ModelViewSet):
     serializer_class = CalendarEventSerializer
     permission_classes = [permissions.IsAuthenticated]
+    queryset = CalendarEvent.objects.all()
 
     def get_queryset(self):
         """
@@ -16,13 +18,18 @@ class CalendarEventViewSet(viewsets.ModelViewSet):
         """
         user = self.request.user
         if user.is_superuser:
-            return CalendarEvent.objects.all()
+            return self.queryset
 
-        # Get teams the user is a member of
-        user_teams = user.player_on_teams.all().union(user.coach_on_teams.all())
+        # Get the actual Team objects the user is a member of.
+        # We use the correct related_names from the Team model.
+        member_of_teams = Team.objects.filter(
+            Q(players=user) | Q(coaches=user)
+        ).distinct()
 
-        return CalendarEvent.objects.filter(
-            Q(team__in=user_teams) | Q(attendees=user)
+        # Filter events where the event's team is in our list of teams,
+        # OR where the user is a direct attendee.
+        return self.queryset.filter(
+            Q(team__in=member_of_teams) | Q(attendees=user)
         ).distinct()
 
     def perform_create(self, serializer):
