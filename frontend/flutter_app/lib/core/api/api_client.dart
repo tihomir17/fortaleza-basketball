3bb3e7813b3d75581cf43e747d8d99bb019c4d8e
Ognjen Ikovic <ikovico@gmail.com>
2025-09-07 11:08:40 +0200

    Frontend production fixes

--- frontend/flutter_app/lib/core/api/api_client.dart
+++ frontend/flutter_app/lib/core/api/api_client.dart
@@ -4,7 +4,11 @@ import 'package:fortaleza_basketball_analytics/main.dart'; // Import for global 
 class ApiClient {
   // For testing with a local Django server on a mobile emulator,
   // use 10.0.2.2 for Android and 127.0.0.1 for iOS/web.
-  static const String baseUrl = "http://127.0.0.1:8000/api";
+  // LOKALNO
+  // static const String baseUrl = "http://127.0.0.1:8000/api";
+  // PRODUKCIJA
+  static const String baseUrl = "https://admin.inatdrive.com/api";
+
   // static const String baseUrl = "http://10.0.2.2:8000/api";
 
   ApiClient() {
